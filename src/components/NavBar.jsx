f7f02028eda3ae7fbd45b304953abcec54f05659
Tanqir Masroor <tanqir.msarker@gmail.com>
2021-05-31 15:43:14 +0600

    prop changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import NavItems from './NavItems';
 
-const NavBar = ({planets, isMenuOpen, setSelectedPlanet, setIsMenuOpen}) => (
-    <div className={`absolute z-20 top-20 w-full transform ${isMenuOpen ? "block translate-x-0" : "-translate-x-full"} bg-cblack h-screen transition ease-in duration-150`}>
-        {planets.map(val => {
+const NavBar = (props) => (
+    <div className={`absolute z-20 top-20 w-full transform ${props.isMenuOpen ? "block translate-x-0" : "-translate-x-full"} bg-cblack h-screen transition ease-in duration-150`}>
+        {props.planets.map(val => {
             return (
                 <div key={val}>
-                    <NavItems name={val} setIsMenuOpen={setIsMenuOpen} setSelectedPlanet={setSelectedPlanet}/>
+                    <NavItems name={val} setIsMenuOpen={props.setIsMenuOpen} setSelectedPlanet={props.setSelectedPlanet}/>
                     <div className="w-full h-px bg-cgray opacity-20"></div>
                 </div>
             );
