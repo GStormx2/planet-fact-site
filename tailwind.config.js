744866eb0f13bed14125678eb9fc70d7f540c529
Tanqir Masroor <tanqir.msarker@gmail.com>
2021-06-01 18:56:13 +0600

    adding purge paths to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+    './public/index.html'
+],
   darkMode: false, // or 'media' or 'class'
   theme: {
     screen: {
