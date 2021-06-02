db3a27a6448d4fa3fa44900928d8c50388279369
Tanqir Masroor <tanqir.msarker@gmail.com>
2021-06-02 16:58:12 +0600

    fixing html errors

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,7 +8,7 @@ import MediumMenu from './MediumMenu';
 const Content = (props) => {
     const info = data.filter(val => val.name === props.selectedPlanet);
     return (
-        <div className="xl:mt-5 xl:grid xl:grid-cols-2 xl:justify-center xl:items-center">
+        <div className="xl:mt-10 xl:grid xl:grid-cols-2 xl:justify-center xl:items-center">
             <PlanetImage selectedMenu={props.selectedMenu} selectedPlanet={props.selectedPlanet}/>
             <div className="md:px-10 md:flex md:items-center md:justify-center xl:flex-col">
                 <PlanetDescription info={info[0]} selectedMenu={props.selectedMenu} selectedPlanet={props.selectedPlanet}/>
--- src/components/PlanetImage.jsx
+++ src/components/PlanetImage.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import {planetSvgs, planetInternalSvg, planetGeologyImage} from './Source';
 
+//width={planetSize[props.selectedPlanet.toLowerCase()]}
 const PlanetImage = (props) => {
-    const planetSize = {
-        'earth'   : '50%',
-        'mercury' : '30%',
-        'venus'   : '45%',
-        'mars'    : '35%',
-        'jupiter' : '65%',
-        'saturn'  : '65%',
-        'uranus'  : '55%',
-        'neptune' : '50%',
-    }
     const planetMaxSize = {
         'earth'   : 'max-w-earth',
         'mercury' : 'max-w-mercury',
@@ -30,22 +21,22 @@ const PlanetImage = (props) => {
     if (props.selectedMenu === 'overview') {
         return (
             <div className="mt-16 md:mt-20 md:mb-20 xl:w-full xl:m-5">
-                <img className={`mx-auto ${planetMaxSize[props.selectedPlanet.toLowerCase()]}`} width={planetSize[props.selectedPlanet.toLowerCase()]} src={svg} alt={`${props.selectedPlanet}`}/>
+                <img className={`${props.selectedPlanet.toLowerCase()} mx-auto ${planetMaxSize[props.selectedPlanet.toLowerCase()]}`} src={svg} alt={`${props.selectedPlanet}`}/>
             </div>
         );
     }
     else if (props.selectedMenu === 'structure') {
         return (
             <div className="mt-16 md:mt-20 md:mb-20 xl:w-full xl:m-5">
-                <img className={`mx-auto ${planetMaxSize[props.selectedPlanet.toLowerCase()]}`} width={planetSize[props.selectedPlanet.toLowerCase()]} src={internal} alt={`${props.selectedPlanet} internal`}/>
+                <img className={`${props.selectedPlanet.toLowerCase()} mx-auto ${planetMaxSize[props.selectedPlanet.toLowerCase()]}`} src={internal} alt={`${props.selectedPlanet} internal`}/>
             </div>
         )
     }
     else if (props.selectedMenu === 'surface') {
         return (
             <div className="relative w-full mb-28 mt-16 md:mt-20 md:mb-32 xl:m-5">
-                <img className={`mx-auto ${planetMaxSize[props.selectedPlanet.toLowerCase()]}`} width={planetSize[props.selectedPlanet.toLowerCase()]} src={svg} alt={`${props.selectedPlanet}`}/>
-                <img className="absolute max-w-pointer left-1/2 transform -translate-x-1/2 -translate-y-1/2" width="25%" src={geology}  alt={`${props.selectedPlanet} geology`}/>
+                <img className={`${props.selectedPlanet.toLowerCase()} mx-auto ${planetMaxSize[props.selectedPlanet.toLowerCase()]}`} src={svg} alt={`${props.selectedPlanet}`}/>
+                <img className="pointer absolute max-w-pointer left-1/2 transform -translate-x-1/2 -translate-y-1/2" src={geology}  alt={`${props.selectedPlanet} geology`}/>
             </div>
         );
     }
