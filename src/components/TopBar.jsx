eb561b655c71fbcba24ce0338ea24241dba5c529
Tanqir Masroor <tanqir.msarker@gmail.com>
2021-06-02 18:31:51 +0600

    adding transition and hover on top nav items

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -5,7 +5,7 @@ import TopNavigationItems from './TopNavigationItems';
 
 const TopBar = (props) => (
     <div>
-        <div className="w-full flex justify-between md:flex-col xl:flex-row items-center p-5 md:p-10 xl:p-0 xl:pl-10 xl:pr-5">
+        <div className="w-full flex justify-between md:flex-col xl:flex-row items-center p-5 md:pt-10 xl:p-0 xl:pl-10 xl:pr-5">
             <h1 className="relative font-antonio text-planets text-cwhite uppercase">The Planets</h1>
             <div className="focus:outline-none md:hidden" onClick={() => props.setIsMenuOpen(!props.isMenuOpen)}>
                 <svg className={props.isMenuOpen ? "opacity-25" : ""}xmlns="http://www.w3.org/2000/svg" width="24" height="17"><g fill="#FFF" fillRule="evenodd"><path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z"/></g></svg>
--- src/components/TopNavigationItems.jsx
+++ src/components/TopNavigationItems.jsx
@@ -3,11 +3,15 @@ import {colorPalletMenu} from './Source';
 
 const TopNavigationItems = (props) => {
     return (
-        <div className={`xl:mr-5 flex-1 flex flex-col justify-between items-center focus:outline-none hover:cursor-pointer}`} 
+        <div className={`xl:mr-5 h-full flex-1 flex flex-col justify-between items-center focus:outline-none}`} 
         onClick={()=> props.setSelectedPlanet(props.name)}>
-            <div className={`hidden xl:block w-20 h-1 ${props.selectedPlanet === props.name ? "visible": "invisible"} ${colorPalletMenu[props.selectedPlanet.toLowerCase()]}`}></div>
-            <p className={`flex-1 mb-5 xl:mt-5 xl:mb-5 xl:m-0 font-spartan font-bold text-tab-md text-cwhite uppercase tracking-widest ${props.selectedPlanet === props.name ? "opacity-100" : "opacity-75"}  hover:cursor-pointer`} >{props.name}</p>
-            <div className={`xl:invisible w-20 h-1 ${props.selectedPlanet === props.name ? "visible": "invisible"} ${colorPalletMenu[props.selectedPlanet.toLowerCase()]}`}></div>
+            <div className={`text-center hover:${colorPalletMenu[props.name.toLowerCase()]} transition duration-150 ease-linear hover:cursor-pointer`}>
+                <div className={`hidden xl:block w-20 h-1 ${props.selectedPlanet === props.name ? "visible": "invisible"} ${colorPalletMenu[props.selectedPlanet.toLowerCase()]}`}></div>
+                <div className={`hidden md:invisible xl:hidden w-20 h-1 ${props.selectedPlanet === props.name ? "visible": "invisible"} ${colorPalletMenu[props.selectedPlanet.toLowerCase()]}`}></div>
+                
+                <p className={`flex-1 mb-5 mt-5 xl:mb-5 font-spartan font-bold text-tab-md text-cwhite uppercase tracking-widest ${props.selectedPlanet === props.name ? "opacity-100" : "opacity-75"}`} >{props.name}</p>
+                <div className={`xl:invisible w-20 h-1 ${props.selectedPlanet === props.name ? "visible": "invisible"} ${colorPalletMenu[props.selectedPlanet.toLowerCase()]}`}></div>
+            </div>
         </div>
     );
 }
